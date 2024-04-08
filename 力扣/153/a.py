f0e07e50e30eb587bd179fcd3711381e9e52dbfb
eya46 <61458340+eya46@users.noreply.github.com>
2024-04-08 17:57:31 +0800

    lc

--- 力扣/153/a.py
+++ 力扣/153/a.py
@@ -3,15 +3,16 @@ from typing import List
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        a=0
-        b=len(nums)-1
-        while a<b:
-            mid = (a+b)//2
-            if nums[mid]>nums[b]:
-                a=mid+1
+        a = 0
+        b = len(nums) - 1
+        while a < b:
+            mid = (a + b) // 2
+            if nums[mid] > nums[b]:
+                a = mid + 1
             else:
-                b=mid
+                b = mid
         return nums[a]
 
+
 if __name__ == "__main__":
-    print(Solution().findMin([4,5,6,7,0,1,2]))
+    print(Solution().findMin([4, 5, 6, 7, 0, 1, 2]))
